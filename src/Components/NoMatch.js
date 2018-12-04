5d6aad773e00e90e0da7097b3b47cd5f601152ac
gavset99@gmail.com <blackcat99>
2018-12-04 09:08:37 -0500

    Added "page not found" page

---
+++ src/Components/NoMatch.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+
+class NoMatch extends Component{
+
+    componentDidMount(){
+
+    }
+
+    render(){
+      return (
+        <div>
+        <h1>Page not found</h1>
+        </div>
+      );
+    }
+
+}
+
+export default NoMatch;
